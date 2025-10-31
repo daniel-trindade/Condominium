28e9a7f844f025edbc1573acae1e4186b1a65b31
daniel-trindade <danielbrunotrindade@gmail.com>
2025-10-31 20:33:59 -0300

    opções de consulta feitas

--- src/pages/condominos/Condominos.jsx
+++ src/pages/condominos/Condominos.jsx
@@ -1,17 +1,81 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import styles from './Condominos.module.css';
 import Container from '../../components/container/Container';
 import ActionButton from '../../components/action_button/ActionButton';
 import consult_cond from '../../assets/consultar_condomino2.png';
+
 import add_cond from '../../assets/adicionar_condomino.png';
 import user_icon from '../../assets/user.png';
-
-
+import v_check from '../../assets/v_check.png';
+  
+const inicialState = {
+  nome: '',
+  telefone: '',
+  cpf: '',
+  data_nascimento: '',
+  bloco: '',
+  apartamento: '',
+  foto: null
+};
 
 function Condominos() {
 
   const  [modo , setModo] = useState('');
+  const [modoConsulta, setModoConsulta] = useState('');
+  const [condomino, setCondomino] = useState(inicialState);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCondomino({
+      ...condomino,
+      [name]: value
+    });
+  };
+
+  const handleChange_MC = (e) => {
+    setModoConsulta(e.target.value);
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+        setCondomino(prevState => ({
+          ...prevState,
+          foto: file 
+        }));
+
+        const imageUrl = URL.createObjectURL(file);
+        setSelectedImage(imageUrl);
+
+      } else {
+
+        setCondomino(prevState => ({
+          ...prevState,
+          foto: null
+        }));
+        setSelectedImage(null);
+
+      }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(condomino);
+    setCondomino(inicialState);
+    setSelectedImage(null);
+    setModo('cadastrado_sucesso');
+  };
+
+  useEffect(() => {
+    return () => {
+      if (selectedImage) {
+        URL.revokeObjectURL(selectedImage);
+      }
+    };
+  }, [selectedImage]);
+
 
   return (
     <Container>
@@ -35,55 +99,145 @@ function Condominos() {
               <div className={styles.img}>
 
                 <h2>Novo Cadastro</h2>
-                <img src={user_icon} alt="" />
-                <input type="file" name="foto" />
+                <img 
+                  src={selectedImage ? selectedImage : user_icon}
+                  alt="Ícone de usuário" 
+                />
+                <input
+                  onChange={handleFileChange}
+                  type="file"
+                  name="foto" 
+                />
 
               </div>
 
-              <form action="" className={styles.form}>
+              <form className={styles.form} onSubmit={handleSubmit}>
                 
                 <div className={styles.formField}>
-                  <label for="nome" >Nome do Condomínio:</label>
-                  <input type="text" name="nome" />
+                  <label for="nome" >Nome do Condômino:</label>
+                  <input 
+                    onChange={handleChange} 
+                    type="text"
+                    name="nome"
+                    id='nome'
+                    value={condomino.nome}
+                    required
+                  />
                 </div>
 
                 <div className={styles.formField}>
                   <label for="telefone">Telefone:</label>
-                  <input type="text" name="telefone" />
+                  <input
+                    onChange={handleChange} 
+                    type="text"
+                    name="telefone"
+                    id='telefone'
+                    value={condomino.telefone}
+                    required
+                  />
                 </div>
 
                 <div className={styles.formField}>
                   <label for="cpf">CPF:</label>
-                  <input type="text" name="cpf" />
+                  <input
+                    onChange={handleChange} 
+                    type="text"
+                    name="cpf"
+                    id='cpf'
+                    value={condomino.cpf}
+                    required
+                  />
                 </div>
 
                 <div className={styles.formField}>
                   <label for="data_nascimento">Data de Nascimento:</label>
-                  <input type="text" name="data_nascimento" />
+                  <input
+                    onChange={handleChange}
+                    type="text"
+                    name="data_nascimento"
+                    id='data_nascimento'
+                    value={condomino.data_nascimento}
+                    required
+                  />
                 </div>
                 
                 <div className={styles.fieldBlock}>
 
                   <div className={styles.formField}>
                     <label for="bloco" >Bloco:</label>
-                    <input type="text" name="bloco" />
+                    <input
+                      onChange={handleChange}
+                      type="text"
+                      name="bloco"
+                      id='bloco'
+                      value={condomino.bloco}
+                      required
+                    />
                   </div>
 
                   <div className={styles.formField}>
                     <label for="apartamento">Apartamento:</label>
-                    <input type="text" name="apartamento" />
+                    <input
+                      onChange={handleChange}
+                      type="text"
+                      name="apartamento"
+                      id='apartamento'
+                      value={condomino.apartamento}
+                      required
+                    />
                   </div>
 
                 </div>
+
+                <button type="submit" className={styles.submitButton}>Cadastrar</button>
 
               </form>
             </div>
           )}
 
           {modo === 'consultar' && (
-            <div className={styles.formArea}>
+            <div className={styles.consultarCondomino}>
+
               <h2>Consultar Condomínios</h2>
-              <p>Lista de condomínios cadastrados.</p>
+
+              <form className={styles.modo_consulta}>
+                <label htmlFor="modo_consulta">Selecione modo de consulta:</label>
+                <select id="modo_consulta" name="modo_consulta" onChange={handleChange_MC} >
+                  <option value="nome">Por Nome</option>
+                  <option value="apartamento">Por Apartamento</option>
+                  <option value="cpf">Por CPF</option>
+                </select>
+              </form>
+              
+              {modoConsulta === 'cpf' && (
+                <div className={styles.successMessage}>
+                  <form>
+                    <label htmlFor="cpf">Digite o CPF:</label>
+                    <input type="text" id="cpf" name="cpf" />
+                    <button type="submit">Consultar</button>
+                  </form>
+                </div>
+              )}
+
+              {modoConsulta === 'nome' && (
+                <div className={styles.successMessage}>
+                  <h2>Consulta por Nome</h2>
+                </div>
+              )}
+
+              {modoConsulta === 'apartamento' && (
+                <div className={styles.successMessage}>
+                  <h2>Consulta por Apartamento</h2>
+                </div>
+              )}
+            </div>
+          )}
+
+          {modo === 'cadastrado_sucesso' && (
+            <div className={styles.successMessage}>
+              <h2>Cadastro Realizado com Sucesso</h2>
+              <p>O condômino foi cadastrado com sucesso.</p>
+              <img src={v_check} alt="Ícone de sucesso" />
             </div>
           )}
         </div>
