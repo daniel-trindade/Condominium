7fb83fc9abd010b60cb703dd8906605b76f1760c
celinehelena <celine.andrade.710@ufrn.edu.br>
2025-12-07 22:22:15 -0300

    Merge parcial

---
+++ frontend/src/AppLayout.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route, useLocation } from 'react-router-dom';  
+
+import NavbarLoginPage from './components/login_navbar/Navbar_login_page';
+import Navbar from './components/login_navbar/Navbar'
+import Footer from './components/footer/Footer'
+import Login from './pages/login/Login'
+import Home from './pages/home/Home'
+import Condominos from './pages/condominos/Condominos';
+import Veiculos from './pages/veiculos/Veiculos';
+import AreaComum from './pages/area_comum/Reservas';
+
+
+
+import './App.css'
+
+function AppLayout() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+
+  return (
+    <>
+      {isLoginPage ? <NavbarLoginPage /> : <Navbar />}
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/condominos' element={<Condominos />} />
+        <Route path='/veiculos' element={<Veiculos />} />
+        <Route path='/area_comum' element={<AreaComum />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default AppLayout
